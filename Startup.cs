f1b1a61d13e0c89e11c470e0b0e928278910561f
azure_devops_codecommit-at-735882332916 <albert.tanure@kapturedocs.com.br>
2019-10-02 23:24:35 -0300

    Alteração api

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace ApiMySql
              */
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "Albert Tanure", Version = "v1" });
             });
 
             /*
